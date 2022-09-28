f82df63cc32f7216edd565f7bdcad4488d44d885
sdodonnell <sodonnell@popsugar.com>
2022-09-27 21:02:17 -0700

    Fix CSV download button

--- components/DownloadButton.jsx
+++ components/DownloadButton.jsx
@@ -2,6 +2,34 @@ import { Button } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { CSVLink } from 'react-csv';
 
+const headers = [
+  { label: 'Company Name', key: 'company' },
+  { label: 'Parent Company', key: 'parentName' },
+  { label: 'Ownership Investor', key: 'owner' },
+  { label: 'Stock Ticker', key: 'stock' },
+  { label: 'Sectors', key: 'sectors' },
+  { label: 'Subsectors', key: 'subsectors' },
+  { label: 'Divestment Target?', key: 'divestment' },
+  { label: 'Prison Labor Involvement', key: 'laborInvolvement' },
+  { label: 'Detention Involvement', key: 'detentionInvolvement' },
+  { label: 'Harm Score', key: 'harmScore' },
+  { label: 'Salience', key: 'salience' },
+  { label: 'Responsibility', key: 'responsibility' },
+  { label: 'Responsiveness', key: 'responsiveness' },
+  { label: 'Active?', key: 'active' },
+  { label: 'Year Founded', key: 'yearFounded' },
+  { label: 'Last Acquired', key: 'acquired' },
+  { label: '# of Employees', key: 'employees' },
+  { label: 'Prision Revenue Only?', key: 'revenueOnly' },
+  { label: 'Capital Markets Exposure', key: 'exposure' },
+  { label: 'Executive', key: 'executive' },
+  { label: 'Revenues', key: 'revenues' },
+  { label: 'Headquarters', key: 'state' },
+  { label: 'Fiscal Year', key: 'fiscalYear' },
+  { label: 'Political Spending', key: 'politicalSpending' },
+  { label: 'Website', key: 'website' },
+];
+
 const DownloadButton = ({ rows }) => {
   const [data, setData] = useState([]);
 
@@ -25,6 +53,7 @@ const DownloadButton = ({ rows }) => {
         asyncOnClick={true}
         onClick={getUsers}
         filename="worth-rises-database.csv"
+        headers={headers}
       >
         Download CSV
       </CSVLink>
