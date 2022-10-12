6632e12f166f59937993f31c4e6d87d1c8f2832e
sdodonnell <sam.odonnell@voxmedia.com>
2022-10-12 10:22:02 -0700

    Attempt to fix social share images

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,11 @@ export default function Document() {
         <meta name="google" content="notranslate" />
         <link rel="shortcut icon" href="/favicon.jpeg" />
         <script type="text/javascript" async src="https://s3.tradingview.com/tv.js" />
-        <meta name="twitter:image" content="/share-image.png" />
+        <meta name="twitter:card" content="summary"></meta>
+        <meta name="twitter:image" content="/share-image.png"></meta>
+        <meta property="og:url" content="https://data.worthrises.org" />
+        <meta property="og:title" content="The Prison Industry Corporate Database" />
+        <meta property="og:description" content="The prison industry is comprised of over 4,000 corporations that profit off of incarceration. These corporations have a vested financial interest in expanding the reach of our carceral system and worsening the incredible harm it causes at the expense of public safety. It’s time we learn their names and get to know them." />
         <meta property="og:image" content="/share-image.png" />
       </Head>
       <body>
